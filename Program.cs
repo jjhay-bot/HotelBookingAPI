68e4ea27ef26982dc6f17060a2c81179e51aa49a
jhay <jhay.alcorcon@gmail.com>
2025-09-07 21:16:36 +0800

    cleanup weather API sample

--- Program.cs
+++ Program.cs
@@ -4,7 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddScoped<WeatherForecastService>();
 builder.Services.AddSingleton<RoomService>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
